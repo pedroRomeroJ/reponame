2c2f1b38d42738cce84c5c0d877f73e153ab03de
Pedro Romero <pjr616039@gmail.com>
2017-10-25 15:23:54 -0500

    First right commit?

---
+++ PedroRomero/src/Student/PedroRomero.java
@@ -0,0 +1,10 @@
+package Student;
+
+public class PedroRomero {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi, I'm Pedro Romero and this is my first commit");
+	}
+
+}
